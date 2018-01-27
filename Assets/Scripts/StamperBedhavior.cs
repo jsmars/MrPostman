3d19a059551ca5390ae455581eb8ae82915f38ec
Erik Lenells <eriklenells@gmail.com>
2018-01-28 00:15:33 +0100

    Stamper update

---
+++ Assets/Scripts/StamperBedhavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StamperBedhavior : MonoBehaviour {
+
+
+    public GameObject StamperStamp;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        var letterEntity = collider.GetComponent<LetterEntity>();
+        if (letterEntity == null)
+        {
+            return;
+        }
+                    
+        var newStamperStamp = Instantiate(StamperStamp, collider.transform);
+        newStamperStamp.transform.position = this.transform.position;
+        newStamperStamp.transform.rotation = this.transform.rotation;
+               
+    }
+}
