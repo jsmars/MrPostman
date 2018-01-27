ec338f6a75ac73246f8e38a1e5a9d00fd32c8030
Andreas Wilcox <andreas.wilcox@m-sys.se>
2018-01-27 21:46:11 +0100

    Started work on highscore list

---
+++ Assets/Scripts/LoadHighscores.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadHighscores : MonoBehaviour
+{
+	public Transform HighscoresPanel;
+	public GameObject HighscoreTemplate;
+
+	public void Start()
+	{
+		var highscores = new List<Highscore>()
+		{
+			new Highscore("SvDvorak", 999),
+			new Highscore("Erik Lenells", 800),
+			new Highscore("Adam Adamsson", 299),
+			new Highscore("ItTotallyRocks!!1!", 231),
+			new Highscore("AnnoyingMan", 100),
+			new Highscore("Man coolio", 49),
+			new Highscore("Super McGamingson", 33),
+			new Highscore("SuperCoolGuy111", 10),
+			new Highscore("!!11Snipah11!!", 9),
+			new Highscore("ThisGameSucks", 1)
+		};
+
+		foreach (var highscore in highscores)
+		{
+			var panel = Instantiate(HighscoreTemplate, HighscoresPanel, false);
+			panel.GetComponent<HighscoreSetter>().Set(highscore);
+		}
+	}
+}
+
+public class Highscore
+{
+	public string Name { get; private set; }
+	public int Score { get; private set; }
+
+	public Highscore(string name, int score)
+	{
+		Score = score;
+		Name = name;
+	}
+}
