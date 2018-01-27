ec52815afccc5eaa11279d68ca471d7049c3fbda
Erik Lenells <eriklenells@gmail.com>
2018-01-27 14:08:53 +0100

    Conveyor belt is mostly working... mostly

--- Assets/Scripts/ConveyorBehavior.cs
+++ Assets/Scripts/ConveyorBehavior.cs
@@ -10,7 +10,7 @@ public class ConveyorBehavior : MonoBehaviour {
 
 	public void Start ()
     {
-		_spawnArea = letterSpawn.GetComponent<Collider>().bounds;
+        _spawnArea = letterSpawn.GetComponent<Collider>().bounds;
         _timeToSpawn = 2;
 	}
 	
@@ -26,10 +26,10 @@ public class ConveyorBehavior : MonoBehaviour {
 
     void SpawnLetter()
     {
-        //var spawnPosition = letterSpawn.position + new Vector3(Random.Range(-0.7f, 0.7f), Random.Range(-1.0f, 1.0f), 0);
+        var spawnPosition = letterSpawn.position + GetRandomPointInSpawnArea();// new Vector3(Random.Range(-0.7f, 0.7f), Random.Range(-1.0f, 1.0f), 0);
         var spawnRotation = Quaternion.Euler(new Vector3(0, Random.Range(0f, 360.0f), 0));
 
-        var letter = (GameObject)Instantiate(letterPrefab, letterSpawn.position, spawnRotation);
+        var letter = (GameObject)Instantiate(letterPrefab, spawnPosition, spawnRotation);
         var randomSpawnTime = Random.Range(_randomNess, _randomNess+1);
         _timeToSpawn = randomSpawnTime;
     }
